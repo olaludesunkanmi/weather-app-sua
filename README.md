d2ef6b295bc39b7e6946d82f03a2c0e3da94f512
Sunkanmi Olalude <olaludesunkanmi@yahoo.com>
2020-08-16 16:33:12 +0100

    weather app up and running

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# weather-app-sua
+#weather-app-sua
+A weather application that interacts with the MapBox and weatherstack APIs to give you real time information about your location's weather forecast.
+
+###Usage
+####Installation
+Install the dependencies
+
+$ npm install
+Run app
+
+$ npm start
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=432fc7bb54ae6cabce0f48691a5dbebb/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
